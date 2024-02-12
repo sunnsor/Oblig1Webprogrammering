e23f590e9d294543ce31b4cc45bd8bbb215326d0
Sunniva Sørensen <susor6866@oslomet.no>
2024-02-12 16:44:42 +0100

    Opprettet prosjekt

---
+++ src/main/java/com/example/oblig1webprogrammering/Oblig1WebprogrammeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1webprogrammering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1WebprogrammeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig1WebprogrammeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oblig1webprogrammering/Oblig1WebprogrammeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1webprogrammering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1WebprogrammeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
